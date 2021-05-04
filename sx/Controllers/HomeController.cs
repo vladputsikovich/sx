66cc62ec5d5c4a34c907f603d013ae75257cdcec
влад пуцыкович <vladputsikovich@gmail.com>
2021-05-04 16:26:41 +0300

    Добавьте файлы проекта.

---
+++ sx/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using sx.Models;
+
+namespace sx.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "admin, user")]
+        public IActionResult Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+            ViewBag.Message = $"ваша роль: {role}";
+            return View("Index");
+        }
+        [Authorize(Roles = "admin")]
+        public IActionResult About()
+        {
+            return Content("Вход только для администратора");
+        }
+    }
+}
---
+++ sx/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sx.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int? RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
