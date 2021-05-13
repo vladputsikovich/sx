65583cbbe609f635e13194cf4573e17251fd4ced
влад пуцыкович <vladputsikovich@gmail.com>
2021-05-13 17:30:04 +0300

    Add fuctional for work with card

---
+++ sx/Controllers/CardController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sx.Models;
+using sx.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Linq;
+namespace sx.Controllers
+{
+    public class CardController : Controller
+    {
+        private AppContext _context;
+
+        public CardController(AppContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Card()
+        {
+            List<Goods> list = new List<Goods>();
+            foreach (var item in HttpContext.Request.Cookies.Where(x=>x.Value == User.Identity.Name))
+            {
+                list.Add(_context.Goods.Where(x => x.Id.ToString() == item.Key).FirstOrDefault());
+            }
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult AddToCard(int id)
+        {
+            HttpContext.Response.Cookies.Append($"{id}",User.Identity.Name);
+            return Redirect("../Goods/AllGoods");
+        }
+    }
+}
--- sx/wwwroot/js/site.js
+++ sx/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(".nums").change(function () {
+    let x = $("#" + this.attributes["id"].value + ".nums").val();
+    let y = $("#" + this.attributes["id"].value + ".price")[0].innerHTML;
+    let count = parseInt(x) * parseInt(y);
+    let fullprice = 0;
+    $("#" + this.attributes["id"].value + ".fullprice")[0].innerHTML = count;
+    $(".fullprice").each(function () {
+        fullprice += parseInt(this.innerText);
+    });
+    $(".count")[0].innerText= fullprice;
+});
+
